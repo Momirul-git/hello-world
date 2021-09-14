5b84c1fbf05a91c43a6d8f809f51eaee004172e2
Momirul-git <90685207+Momirul-git@users.noreply.github.com>
2021-09-15 00:14:40 +0530

    Largestnum  file created

---
+++ Largestnum.java
@@ -0,0 +1,36 @@
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=30;
+		int b=45;
+		int c=66;
+		 
+		if(a>b)
+		{
+			if(a>c)
+			{
+				System.out.println("A is the gretest");
+			}
+			else {
+				System.out.println("C is the gretest");
+			}
+			
+		}
+		else if(b>a)
+		{
+			if(b>c)
+			{
+				System.out.println("B is the gretest");
+			}
+			else
+			{
+				System.out.println("C is the gretest");
+			}
+			
+		}
+	}
+				 
+		 
+	}
+	
